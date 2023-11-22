ee49a7f6e8ebbfd7970bc7bd41c6e8ce2ad96c1e
Magdalena <73961046+MagdalenaPakula@users.noreply.github.com>
2023-11-22 12:06:36 +0100

    Update app.module.ts

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -10,16 +10,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
 import { MatMenuModule } from '@angular/material/menu';
-import {SignupComponent} from "./signup/signup.component";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import { SignupComponent } from "./signup/signup.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent
   ],
     imports: [
         BrowserModule,
