d22b696bd0de29d6e4899cea25dea87884aca092
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2023-11-27 22:56:12 +0100

    whether i am user or admin i get redirecte to different pages

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,25 +1,27 @@
+// login.component.ts
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {UserService} from "../services/user.service";
-import {Router} from "@angular/router";
-import {DialogService} from "../services/dialog.service";
-import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
-import {ForgotPasswordComponent} from "../forgot-password/forgot-password.component";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { UserService } from "../services/user.service";
+import { Router } from "@angular/router";
+import { DialogService } from "../services/dialog.service";
+import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
+import { ForgotPasswordComponent } from "../forgot-password/forgot-password.component";
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private userService: UserService, private router: Router, private dialogService: DialogService, private dialog:MatDialog) {
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router, private dialogService: DialogService, private dialog: MatDialog) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password:['', [Validators.required]]
-    })
+      password: ['', [Validators.required]]
+    });
   }
 
   login() {
@@ -31,10 +33,21 @@ export class LoginComponent {
     const loginData = this.loginForm.value;
 
     this.userService.login(loginData).subscribe(
-        (response) => {
+        (response: any) => {
           console.log('Successfully logged in:', response);
           this.dialogService.openDialog('Login Status', 'Successfully logged in');
-          this.router.navigate(['/home']);
+
+          const token = response.token;
+
+          // decode the token to get user role
+          const decodedToken = this.decodeToken(token);
+          const userRole = decodedToken.role;
+
+          if (userRole === 'admin') {
+            this.router.navigate(['/dashboard']);
+          } else if (userRole === 'user') {
+            this.router.navigate(['/home']);
+          }
         },
         (error) => {
           console.error('Error logging in:', error);
@@ -42,7 +55,13 @@ export class LoginComponent {
         }
     );
   }
-  handleForgotPasswordAction(){
+
+  decodeToken(token: string): any {
+    const jwtHelper = new JwtHelperService();
+    return jwtHelper.decodeToken(token);
+  }
+
+  handleForgotPasswordAction() {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.width = "600px";
     this.dialog.open(ForgotPasswordComponent, dialogConfig);
