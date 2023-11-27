25e2253c7f4eef6e3df0ba1bdeeaf5df6a1445d8
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2023-11-27 14:08:22 +0100

    snanckabr added

--- frontend/src/app/services/snackbar.service.ts
+++ frontend/src/app/services/snackbar.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SnackbarService {
 
-  constructor() { }
+  constructor(private snackBar:MatSnackBar) { }
+
+  openSnackBar(message: string, action: string){
+    if( action === 'error'){
+      this.snackBar.open(message, '', {
+        horizontalPosition:'center',
+        verticalPosition:'top',
+        duration:2000,
+        panelClass:['']
+      })
+    }else{
+
+    }
+  }
 }
