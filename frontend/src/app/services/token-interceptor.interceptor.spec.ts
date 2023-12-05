41acf86284d41eefa2a97e72dea0bfae6f204da4
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2023-12-05 16:45:38 +0100

    authentication REALLY WORKS NOW BRUH !!!!

---
+++ frontend/src/app/services/token-interceptor.interceptor.spec.ts
@@ -0,0 +1,40 @@
+import { TestBed } from '@angular/core/testing';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest} from '@angular/common/http';
+
+import { TokenInterceptorInterceptor } from './token-interceptor.interceptor';
+import {Observable} from "rxjs";
+import {Router} from "@angular/router";
+
+class MockRouter {
+  navigate(path: string[]) {}
+}
+
+describe('TokenInterceptorInterceptor', () => {
+  let interceptor: HttpInterceptor;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        TokenInterceptorInterceptor,
+        { provide: Router, useClass: MockRouter },
+      ],
+    });
+
+    interceptor = TestBed.inject(TokenInterceptorInterceptor);
+  });
+
+  it('should be created', () => {
+    expect(interceptor).toBeTruthy();
+  });
+
+  it('should intercept and modify request', () => {
+    const req = new HttpRequest('GET', '/api/data');
+    const next: HttpHandler = {
+      handle: (request: HttpRequest<any>): Observable<HttpEvent<any>> => {
+        // Your custom logic to handle the request
+        return new Observable(); // Replace with your actual logic
+      },
+    };
+  });
+
+});
---
+++ frontend/src/app/services/token-interceptor.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AuthService} from "./auth.service";
+
+@Injectable()
+export class TokenInterceptorInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken();
+    console.log('Token:', token);
+
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
